ec62ae3396bfa7c7433dc41be28c14f7eac1f210
AkRampersad <akashjrampersad@gmail.com>
2023-05-04 21:19:23 -0400

    parquetreporter 1.0.0 push
    A 1.0.0 push with correct leading zeros for file names depending on amount of files produced, updated yml file, and .py has been formatted using py.black

--- reporters/parquetreporter/parquetreporter.py
+++ reporters/parquetreporter/parquetreporter.py
@@ -6,11 +6,12 @@ import pyarrow.parquet as pq
 import duckdb as db
 import zipfile
 
-class Reporter(BaseReporter): 
+
+class Reporter(BaseReporter):
     def setup(self):
         self.wf = None
         self.filenames = []
-        #list of dict -> parquet file 
+        # list of dict -> parquet file
         self.filename = None
         self.filename_prefix = None
         self.chunkno = 0
@@ -18,10 +19,7 @@ class Reporter(BaseReporter):
         self.batch = []
         self.rowno = 1
         self.list_tbl = []
-        
 
-
-        
         if self.savepath == None:
             self.filename_prefix = "cravat_result"
         else:
@@ -30,13 +28,12 @@ class Reporter(BaseReporter):
             self.confs.get("pages", "variant")
         )
 
-        #for now use sqlite3 package to get total variant row count and store it for chunking purposes
+        # for now use sqlite3 package to get total variant row count and store it for chunking purposes
         sql_conn = sqlite3.connect(self.dbpath)
         curs = sql_conn.cursor()
         curs.execute('SELECT COUNT(*) from "variant"')
         self.results = curs.fetchone()[0]
-    
-
+        self.leading_zeros = len(str(self.results // self.chunk_size))
 
         self.zip = (
             self.get_standardized_module_option(self.confs.get("zip", "false")) == True
@@ -44,16 +41,15 @@ class Reporter(BaseReporter):
         self.module_col_sep = "."
         self.filename_postfix = ".parquet"
 
-
-    def should_write_level(self,level):
+    def should_write_level(self, level):
         if self.levels_to_write is None:
             return True
         elif level in self.levels_to_write:
             return True
         else:
             return False
-          
-    def end(self): 
+
+    def end(self):
         if self.wf is not None:
             self.wf.close()
         if self.zip and self.filename_prefix:
@@ -68,51 +64,43 @@ class Reporter(BaseReporter):
             zf.close()
         else:
             zipfile_path = self.filenames
-        
+
         return self.savepath
-    
 
-
-    def write_preface(self,level):
-
-        #determine if current level needs to be written, if not return
+    def write_preface(self, level):
+        # determine if current level needs to be written, if not return
         if level not in self.levels_to_write:
             return
         if self.wf is not None:
             self.wf.close()
 
-        
-
-    def write_table_row(self,row):
+    def write_table_row(self, row):
         self.batch.append(row)
-        
 
         if self.rowno % self.chunk_size == 0 or self.rowno == self.results:
-            
-            
-            #set the file name with correct chunk number
-            self.filename = f"{self.filename_prefix}{self.chunkno}{self.filename_postfix}"
+            # set the file name with correct chunk number
+
+            self.filename = f"{self.filename_prefix}_{self.chunkno:0{self.leading_zeros}}{self.filename_postfix}"
             self.filenames.append(self.filename)
 
-            #connect to duckDB            
+            # connect to duckDB
             conn = db.connect()
 
-            #create a pyarrow table so that schema is set for duckDB table
+            # create a pyarrow table so that schema is set for duckDB table
             tbl = pa.Table.from_pylist(self.batch)
             tbl_name = f"my_tbl{self.chunkno}"
 
-            
-
-            #create a duckDB table from pyarrow table 
+            # create a duckDB table from pyarrow table
             conn.sql(f"CREATE TABLE {tbl_name} AS SELECT * from tbl")
-            
 
-            #create parquet file from duckDB table 
-            result = conn.execute(f"COPY {tbl_name} to '{self.filename}' (FORMAT 'PARQUET')")
+            # create parquet file from duckDB table
+            result = conn.execute(
+                f"COPY {tbl_name} to '{self.filename}' (FORMAT 'PARQUET')"
+            )
 
             self.list_tbl.append(tbl_name)
 
-            #reset parameters for next chunk
+            # reset parameters for next chunk
             conn.close()
             self.batch = []
 
