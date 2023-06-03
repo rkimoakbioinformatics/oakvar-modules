d478bcb3e67612f0c46cf9e6c5dd6cb0cb835992
Iulian Vlad <ichim.iulian12@gmail.com>
2023-06-04 02:08:36 +0300

    processed new columns

--- reporters/mafreporter/mafreporter.py
+++ reporters/mafreporter/mafreporter.py
@@ -6,7 +6,7 @@ from oakvar import BaseReporter
 class Reporter(BaseReporter):
     MAF_COLUMN_MAP = {
         "Hugo_Symbol": "base__hugo",
-        "Entrez_Gene_Id": "",
+        "Entrez_Gene_Id": "ncbigene__entrez",
         "Center": "null",
         "NCBI_Build": "GRCh38",
         "Chromosome": "base__chrom",
@@ -14,11 +14,11 @@ class Reporter(BaseReporter):
         "End_Position": "base__pos_end",
         "Strand": "+",
         "Variant_Classification": "base__so",
-        "Variant_Type": "special_case",
+        "Variant_Type": "",
         "Reference_Allele": "base__ref_base",
         "Tumor_Seq_Allele1": "base__alt_base",
         "Tumor_Seq_Allele2": "base__alt_base",
-        "dbSNP_RS": "dbsnp__rsid",
+        "dbSNP_RS": "",
         "dbSNP_Val_Status": "",
         "Tumor_Sample_Barcode": "tagsampler__samples",
         "Matched_Norm_Sample_Barcode": "null",
@@ -67,7 +67,7 @@ class Reporter(BaseReporter):
         "DISTANCE": "null",
         "TRANSCRIPT_STRAND": "+",
         "SYMBOL": "base__hugo",
-        "SYMBOL_SOURCE": "special_case",
+        "SYMBOL_SOURCE": "",
         "HGNC_ID": "base__hugo",
         "BIOTYPE": "",
         "CANONICAL": "null",
@@ -77,7 +77,7 @@ class Reporter(BaseReporter):
         "TREMBL": "null",
         "UNIPARC": "null",
         "RefSeq": "base__refseq",
-        "SIFT": "special_case",
+        "SIFT": "",
         "PolyPhen": "",
         "EXON": "base__exonno",
         "INTRON": "null",
@@ -85,7 +85,7 @@ class Reporter(BaseReporter):
         "GMAF": "thousandgenomes__af",
         "AFR_MAF": "thousandgenomes__afr_af",
         "AMR_MAF": "thousandgenomes__amr_af",
-        "ASN_MAF": "special_case",
+        "ASN_MAF": "",
         "EAS_MAF": "thousandgenomes__eas_af",
         "EUR_MAF": "thousandgenomes__eur_af",
         "SAS_MAF": "thousandgenomes__sas_af",
@@ -134,11 +134,48 @@ class Reporter(BaseReporter):
     }
 
     # the columns that need to be skipped/deleted from the final file, if the 'somatic' type is selected.
-    PROTECTED_COLS_TO_DELETE = ['vcf_region', 'vcf_info', 'vcf_format', 'vcf_tumor_gt', 'vcf_normal_gt']
+    PROTECTED_COLS_TO_DELETE = ('vcf_region', 'vcf_info', 'vcf_format', 'vcf_tumor_gt',
+                                'vcf_normal_gt', 'GDC_Valid_Somatic')
 
     # the columns that need to be cleared (set as empty) in the final file, if the 'somatic' type is selected.
-    PROTECTED_COLS_TO_EMPTY = ['Match_Norm_Seq_Allele1', 'Match_Norm_Validation_Allele1',
-                               'Match_Norm_Validation_Allele2', 'n_ref_count', 'n_alt_count']
+    PROTECTED_COLS_TO_EMPTY = ('Match_Norm_Seq_Allele1', 'Match_Norm_Validation_Allele1',
+                               'Match_Norm_Validation_Allele2', 'n_ref_count', 'n_alt_count')
+
+    SPECIAL_CASE_COLS = ('Entrez_Gene_Id', 'Variant_Classification', 'Variant_Type', 'Tumor_Seq_Allele1',
+                         'Tumor_Seq_Allele2', 'dbSNP_RS', 'dbSNP_Val_Status', 'Tumor_Sample_Barcode', 'Amino_acids',
+                         'Existing_variation', 'SYMBOL_SOURCE', 'SWISSPROT', 'PolyPhen', 'VARIANT_CLASS',
+                         'ExAC_AF_Adj', 'FILTER', 'COSMIC', 'vcf_region', 'vcf_info', 'vcf_format',
+                         'vcf_tumor_gt', 'vcf_normal_gt', 'NCBI_Build', 'TRANSCRIPT_STRAND', 'MINIMISED', 'Strand',
+                         'SIFT', 'ASN_MAF', 'all_effects', 'SIFT')
+
+    # TODO: find all base__so equivalents if possible
+    SO_TO_MAF_VARIANT_CLASSIFICATION = {
+        '3_prime_UTR_variant': "3'UTR",
+        '2': "3'Flank",
+        '5_prime_UTR_variant': "5'UTR",
+        '4': "5'Flank",
+        '5': 'IGR',
+        'intron_variant': 'Intron',  # check if true?
+        '7': 'Frame_Shift_Del',
+        '8': 'Frame_Shift_Ins',
+        'inframe_deletion': 'In_Frame_Del',
+        'inframe_insertion': 'In_Frame_Ins',
+        'missense_variant': 'Missense_Mutation',
+        '12': 'Nonsense_Mutation',
+        '13': 'Silent',
+        'splice_site_variant': 'Splice_Site',
+        '15': 'Translation_Start_Site',
+        '16': 'Nonstop_Mutation',
+        '17': 'RNA',
+        '18': 'Targeted_Region',
+        '19': 'De_novo_Start_InFrame',
+        '20': 'De_novo_Start_OutOfFrame'
+    }
+
+    MAF_VARIANT_TYPE = ('SNP', 'DNP', 'TNP', 'ONP', 'INS', 'DEL', 'Consolidated')
+
+    MAF_ALL_EFFECTS = ('SYMBOL', 'Consequence', 'HGVSp_Short', 'Transcript_ID', 'RefSeq', 'HGVSc', 'IMPACT',
+                       'CANONICAL', 'SIFT', 'PolyPhen', 'Strand')
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -149,7 +186,8 @@ class Reporter(BaseReporter):
         self.maf_type = 'somatic'
         self.filename_prefix = None
         self.filename = None
-        self.extension = '.maf'
+        # for test purposes it is .tsv - to be removed in the future
+        self.extension = '.maf.tsv'
         self.headers = None
 
     def setup(self):
@@ -160,6 +198,15 @@ class Reporter(BaseReporter):
             self.filename_prefix = self.maf_type
 
         self.levels_to_write = ['variant']
+
+    def set_maf_type(self):
+        if 'type' in self.module_options:
+            maf_type = self.module_options['type']
+            if maf_type not in ['protected', 'somatic']:
+                # if module type is given wrongly, defaults to somatic
+                self.maf_type = 'somatic'
+            else:
+                self.maf_type = maf_type
 
     def should_write_level(self, level):
         if level != 'variant':
@@ -180,36 +227,185 @@ class Reporter(BaseReporter):
         if self.maf_type == 'protected':
             self.file_writer.writerow(self.headers)
         else:
-            # somatic files are identical to protected files, except that they have the last 6 columns removed
+            # first step to creating Somatic MAF files
+            # TODO: after protected maf is done, somatic will require some filtration still
             self.file_writer.writerow(self.headers[:-6])
 
     def write_table_row(self, row):
-        # col_names = self.extracted_col_names[self.level]
-        new_line = []
-
+        new_row = []
         for col, val in self.MAF_COLUMN_MAP.items():
-            if val in ['', 'null']:
-                new_line.append('')
-
-            elif val in ['null', 'GRCh38', '+', 'special_case', '1']:
-                new_line.append(val)
-
-            elif col in self.PROTECTED_COLS_TO_EMPTY:
-                new_line.append('')
+            if col in self.PROTECTED_COLS_TO_EMPTY:
+                new_row.append('')
+            # checks if column is a special case and handles accordingly
+            elif col in self.SPECIAL_CASE_COLS:
+                proc_value = self.handle_special_cases(row, col)
+                new_row.append(proc_value)
+            elif val in ['', 'null']:
+                new_row.append('')
             else:
-                new_line.append(row[val])
+                new_row.append(row[val])
 
-        self.file_writer.writerow(new_line)
+        self.file_writer.writerow(new_row)
 
-    def set_maf_type(self):
-        if 'type' in self.module_options:
-            maf_type = self.module_options['type']
-            if maf_type not in ['protected', 'somatic']:
-                # if module type is given wrongly, defaults to somatic
-                self.maf_type = 'somatic'
+    def handle_special_cases(self, row, col):
+        """
+        Main function to handle rows which do not contain straight-forward data
+        :param row: database row to handle
+        :param col: database column
+        :return:
+        """
+        if col == 'Entrez_Gene_Id':
+            return row['ncbigene__entrez']
+
+        if col == 'NCBI_Build':
+            return 'GRCh38'
+
+        if col in ['Strand', 'TRANSCRIPT_STRAND']:
+            return '+'
+
+        if col == 'MINIMISED':
+            return '1'
+
+        # TODO: after all is finished, check the notes for
+        #   'base__so values without a MAF mapping can be converted by capitalizing'
+        if col == 'Variant_Classification':
+            return row['base__so']
+
+        if col == 'Variant_Type':
+            variant_ref_base = row['base__ref_base']
+            variant_alt_base = row['base__alt_base']
+            return self.write_variant_type(variant_ref_base, variant_alt_base)
+
+        if col in ['Tumor_Seq_Allele1', 'Tumor_Seq_Allele2']:
+            return row['base__alt_base']
+
+        if col in ['dbSNP_RS', 'dbSNP_Val_Status']:
+            return self.write_dbsnp(row, col)
+
+        if col == 'Tumor_Sample_Barcode':
+            return row['tagsampler__samples']
+
+        if col == 'SYMBOL_SOURCE':
+            return 'HUGO'
+
+        # this is different from what is shown in the variant table because of documentation rules
+        # TODO: check if this should be like this
+        if col == 'SIFT':
+            return self.write_sift(row)
+
+        # used the polyphen2 HDIV algorithm data. Multiple sources seem to have the same rules for both algorithms
+        # TODO: research more which should be used.
+        if col == 'PolyPhen':
+            return self.write_polyphen2(row)
+
+        # TODO: seems to depend on values that are handled AFTER this is run - somehow this has to grab
+        #   values and join them in their final state
+        if col == 'all_effects':
+            return self.write_all_effects(row)
+
+    @staticmethod
+    def write_variant_type(ref_base, alt_base):
+        len_ref_base = len(ref_base)
+        len_alt_base = len(alt_base)
+        variant_type = ''
+
+        if len_ref_base == len_alt_base == 1 and ref_base != alt_base and ref_base != alt_base != '-':
+            variant_type = 'SNP'
+        elif len_ref_base == len_alt_base == 2 and ref_base != alt_base and ref_base != alt_base != '-':
+            variant_type = 'DNP'
+        elif len_ref_base == len_alt_base == 3 and ref_base != alt_base and ref_base != alt_base != '-':
+            variant_type = 'TNP'
+        elif len_ref_base == len_alt_base > 3 and ref_base != alt_base and ref_base != alt_base != '-':
+            variant_type = 'ONP'
+        elif (len_ref_base > len_alt_base) or (ref_base != '-' and alt_base == '-'):
+            variant_type = 'DEL'
+        elif (len_ref_base < len_alt_base) or (alt_base != '-' and ref_base == '-'):
+            variant_type = 'INS'
+
+        return variant_type
+
+    # TODO check the documentation rule here: how would I find if it is in different databases to add 'novel' or 'null'?
+    @staticmethod
+    def write_dbsnp(row, col):
+        if col == 'dbSNP_RS':
+            val = row['dbsnp__rsid']
+            # reverting to null at the moment
+            if val is None:
+                return 'null'
             else:
-                self.maf_type = maf_type
+                return val
+
+        if col == 'dbSNP_Val_Status':
+            return ''
+
+    def write_all_effects(self, row):
+        effects = []
+
+        for effect in self.MAF_ALL_EFFECTS:
+            if self.MAF_COLUMN_MAP[effect] in row:
+                result_row = row[self.MAF_COLUMN_MAP[effect]]
+
+                if result_row is None:
+                    effects.append('')
+                else:
+                    effects.append(result_row)
+            else:
+                effects.append('')
+
+        all_effects = f'[{str(";".join(effects))}]'
+
+        return all_effects
+
+    @staticmethod
+    def write_sift(row):
+        prediction = row['sift__prediction']
+        score = row['sift__score']
+        confidence = row['sift__confidence']
+        sift_values_map = {
+            'Tolerated': 'tolerated',
+            'Damaging': 'deleterious',
+            'Low': '_low_confidence',
+            'High': ''
+        }
+
+        if prediction is None:
+            return ''
+
+        prediction_score = f'{sift_values_map[prediction]}{sift_values_map[confidence]}'
+
+        if score is not None:
+            prediction_score += f'({score})'
+
+        return prediction_score
+
+    @staticmethod
+    def write_polyphen2(row):
+        prediction = row['polyphen2__hdiv_pred']
+
+        if row['polyphen2__hdiv_rank'] is not None:
+            rank = float(row['polyphen2__hdiv_rank'])
+        else:
+            rank = ''
+
+        polyphen_values_map = {
+            'D': 'probably_damaging',
+            'P': 'possibly_damaging',
+            'B': 'benign'
+        }
+        prediction_rank = ''
+
+        if prediction is None:
+            return ''
+
+        # TODO: the check seems to be in variant anyway, the conditionals are redundant
+        if rank >= 0.957:
+            prediction_rank = f'{polyphen_values_map[prediction]}({rank})'
+        elif 0.453 <= rank <= 0.956:
+            prediction_rank = f'{polyphen_values_map[prediction]}({rank})'
+        elif rank < 0.453:
+            prediction_rank = f'{polyphen_values_map[prediction]}({rank})'
+
+        return prediction_rank
 
     def end(self):
         pass
-
