d48ed4904533e9e168c87904f4b916bc17b4b9db
Iulian Vlad <ichim.iulian12@gmail.com>
2023-06-05 01:18:57 +0300

    fixed the way rows are written (now is dict). Added additional columns

--- reporters/mafreporter/mafreporter.py
+++ reporters/mafreporter/mafreporter.py
@@ -19,7 +19,7 @@ class Reporter(BaseReporter):
         "Tumor_Seq_Allele1": "base__alt_base",
         "Tumor_Seq_Allele2": "base__alt_base",
         "dbSNP_RS": "",
-        "dbSNP_Val_Status": "",
+        "dbSNP_Val_Status": "null",
         "Tumor_Sample_Barcode": "tagsampler__samples",
         "Matched_Norm_Sample_Barcode": "null",
         "Match_Norm_Seq_Allele1": "null",
@@ -56,13 +56,14 @@ class Reporter(BaseReporter):
         "Feature": "",
         "Feature_type": "null",
         "One_Consequence": "base__so",
-        "Consequence": "base__so",
+        "Consequence": "",
         "cDNA_position": "null",
         "CDS_position": "base__cchange",
         "Protein_position": "base__achange",
         "Amino_acids": "",
         "Codons": "null",
-        "Existing_variation": "",
+        "Existing_variation": "dbsnp__rsid",  # TODO examples say this would be correct, but recheck. In examples
+        #     there are multiple values separated by ';'
         "ALLELE_NUM": "null",
         "DISTANCE": "null",
         "TRANSCRIPT_STRAND": "+",
@@ -142,11 +143,11 @@ class Reporter(BaseReporter):
                                'Match_Norm_Validation_Allele2', 'n_ref_count', 'n_alt_count')
 
     SPECIAL_CASE_COLS = ('Entrez_Gene_Id', 'Variant_Classification', 'Variant_Type', 'Tumor_Seq_Allele1',
-                         'Tumor_Seq_Allele2', 'dbSNP_RS', 'dbSNP_Val_Status', 'Tumor_Sample_Barcode', 'Amino_acids',
-                         'Existing_variation', 'SYMBOL_SOURCE', 'SWISSPROT', 'PolyPhen', 'VARIANT_CLASS',
-                         'ExAC_AF_Adj', 'FILTER', 'COSMIC', 'vcf_region', 'vcf_info', 'vcf_format',
+                         'Tumor_Seq_Allele2', 'dbSNP_RS', 'Tumor_Sample_Barcode', 'Amino_acids',
+                         'SYMBOL_SOURCE', 'SWISSPROT', 'PolyPhen', 'VARIANT_CLASS',
+                         'ExAC_AF_Adj', 'FILTER', 'vcf_region', 'vcf_info', 'vcf_format',
                          'vcf_tumor_gt', 'vcf_normal_gt', 'NCBI_Build', 'TRANSCRIPT_STRAND', 'MINIMISED', 'Strand',
-                         'SIFT', 'ASN_MAF', 'all_effects', 'SIFT')
+                         'SIFT', 'ASN_MAF', 'all_effects', 'SIFT', 'Consequence')
 
     # TODO: find all base__so equivalents if possible
     SO_TO_MAF_VARIANT_CLASSIFICATION = {
@@ -156,17 +157,17 @@ class Reporter(BaseReporter):
         '4': "5'Flank",
         '5': 'IGR',
         'intron_variant': 'Intron',  # check if true?
-        '7': 'Frame_Shift_Del',
-        '8': 'Frame_Shift_Ins',
+        'frameshift_truncation': 'Frame_Shift_Del',
+        'frameshift_elongation': 'Frame_Shift_Ins',
         'inframe_deletion': 'In_Frame_Del',
         'inframe_insertion': 'In_Frame_Ins',
         'missense_variant': 'Missense_Mutation',
         '12': 'Nonsense_Mutation',
         '13': 'Silent',
-        'splice_site_variant': 'Splice_Site',
+        'splice_site_variant': 'Splice_Site',  # check if true?
         '15': 'Translation_Start_Site',
         '16': 'Nonstop_Mutation',
-        '17': 'RNA',
+        '17': 'RNA',  # which RNA, there are multiple possibilities
         '18': 'Targeted_Region',
         '19': 'De_novo_Start_InFrame',
         '20': 'De_novo_Start_OutOfFrame'
@@ -176,6 +177,78 @@ class Reporter(BaseReporter):
 
     MAF_ALL_EFFECTS = ('SYMBOL', 'Consequence', 'HGVSp_Short', 'Transcript_ID', 'RefSeq', 'HGVSc', 'IMPACT',
                        'CANONICAL', 'SIFT', 'PolyPhen', 'Strand')
+
+    SO_TO_BIOTYPE = {
+        'processed_transcript': 'SO_PTR',
+        'transcribed_unprocessed_pseudogene': 'SO_TU1',
+        'unprocessed_pseudogene': 'SO_UNP',
+        'miRNA': 'SO_MIR',
+        'lncRNA': 'SO_LNC',
+        'processed_pseudogene': 'SO_PPS',
+        'snRNA': 'SO_SNR',
+        'transcribed_processed_pseudogene': 'SO_TPR',
+        'retained_intron': 'SO_RTI',
+        'nonsense_mediated_decay': 'SO_NMD',
+        'misc_RNA': 'SO_MCR',
+        'TEC': 'SO_UNT',
+        'pseudogene': 'SO_PSE',
+        'transcribed_unitary_pseudogene': 'SO_TU2',
+        'non_stop_decay': 'SO_NSD',
+        'snoRNA': 'SO_SNO',
+        'scaRNA': 'SO_SCA',
+        'rRNA_pseudogene': 'SO_PRR',
+        'unitary_pseudogene': 'SO_UPG',
+        'polymorphic_pseudogene': 'SO_PPG',
+        'rRNA': 'SO_RRN',
+        'IG_V_pseudogene': 'SO_IVP',
+        'ribozyme': 'SO_RIB',
+        'sRNA': 'SO_SRN',
+        'TR_V_gene': 'SO_TVG',
+        'TR_V_pseudogene': 'SO_TVP',
+        'TR_D_gene': 'SO_TDG',
+        'TR_J_gene': 'SO_TJG',
+        'TR_C_gene': 'SO_TCG',
+        'TR_J_pseudogene': 'SO_TJP',
+        'IG_C_gene': 'SO_ICG',
+        'IG_C_pseudogene': 'SO_ICP',
+        'IG_J_gene': 'SO_IJG',
+        'IG_J_pseudogene': 'SO_IJP',
+        'IG_D_gene': 'SO_IDG',
+        'IG_V_gene': 'SO_IVG',
+        'IG_pseudogene': 'SO_IGP',
+        'translated_processed_pseudogene': 'SO_TPP',
+        'scRNA': 'SO_SCR',
+        'vaultRNA': 'SO_VLR',
+        'translated_unprocessed_pseudogene': 'SO_TUP',
+        'Mt_tRNA': 'SO_MTR',
+        'Mt_rRNA': 'SO_MRR',
+        'IG_LV_gene': '',
+        'Mt_tRNA_pseudogene': '',
+        'tRNA_pseudogene': '',
+        'snoRNA_pseudogene': '',
+        'snRNA_pseudogene': '',
+        'scRNA_pseudogene': '',
+        'misc_RNA_pseudogene': '',
+        'miRNA_pseudogene': '',
+        'protein_coding': '',
+        'protein_coding_LoF': '',
+        'protein_coding_CDS_not_defined': '',
+        'non_coding': '',
+        'ambiguous_orf': '',
+        'sense_intronic': '',
+        'sense_overlapping': '',
+        'antisense': '',
+        'antisense_RNA': '',
+        'known_ncrna': '',
+        'retrotransposed': '',
+        'artifact': '',
+        'lincRNA': '',
+        'macro_lncRNA': '',
+        '3prime_overlapping_ncRNA': '',
+        'disrupted_domain': '',
+        'vault_RNA': '',
+        'bidirectional_promoter_lncRNA': '',
+    }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
@@ -219,31 +292,39 @@ class Reporter(BaseReporter):
             self.maf_file.close()
         else:
             self.maf_file = open(self.filename, "w", encoding="utf-8", newline="")
-            self.file_writer = csv.writer(self.maf_file, delimiter='\t')
+            self.file_writer = csv.DictWriter(self.maf_file, delimiter='\t', fieldnames='')
 
     def write_header(self, level):
-        self.headers = list(self.MAF_COLUMN_MAP.keys())
 
         if self.maf_type == 'protected':
-            self.file_writer.writerow(self.headers)
+            self.file_writer.fieldnames = self.MAF_COLUMN_MAP.keys()
+
+            self.file_writer.writeheader()
         else:
             # first step to creating Somatic MAF files
             # TODO: after protected maf is done, somatic will require some filtration still
-            self.file_writer.writerow(self.headers[:-6])
+            column_map = self.MAF_COLUMN_MAP
+
+            for col_to_del in self.PROTECTED_COLS_TO_DELETE:
+                column_map.pop(col_to_del)
+
+            self.file_writer.fieldnames = column_map.keys()
+
+            self.file_writer.writeheader()
 
     def write_table_row(self, row):
-        new_row = []
+        new_row = {}
         for col, val in self.MAF_COLUMN_MAP.items():
             if col in self.PROTECTED_COLS_TO_EMPTY:
-                new_row.append('')
+                new_row[col] = ''
             # checks if column is a special case and handles accordingly
             elif col in self.SPECIAL_CASE_COLS:
                 proc_value = self.handle_special_cases(row, col)
-                new_row.append(proc_value)
+                new_row[col] = proc_value
             elif val in ['', 'null']:
-                new_row.append('')
+                new_row[col] = ''
             else:
-                new_row.append(row[val])
+                new_row[col] = row[val]
 
         self.file_writer.writerow(new_row)
 
@@ -268,22 +349,30 @@ class Reporter(BaseReporter):
 
         # TODO: after all is finished, check the notes for
         #   'base__so values without a MAF mapping can be converted by capitalizing'
+        # as stated above, not found values are capitalized
         if col == 'Variant_Classification':
-            return row['base__so']
+            return self.write_variant_classification(row)
 
+        # TODO: Undocumented case: 'Consolidated' option for column Variant_Type
         if col == 'Variant_Type':
             variant_ref_base = row['base__ref_base']
             variant_alt_base = row['base__alt_base']
             return self.write_variant_type(variant_ref_base, variant_alt_base)
 
+        # TODO: document the case covered by note 3
         if col in ['Tumor_Seq_Allele1', 'Tumor_Seq_Allele2']:
-            return row['base__alt_base']
+            return self.write_tumor_seq_alleles(row, col)
 
-        if col in ['dbSNP_RS', 'dbSNP_Val_Status']:
-            return self.write_dbsnp(row, col)
+        # TODO: here also used to be the former special case 'dbSNP_Val_Status'
+        #   for the moment I will leave it empty as I can not find references for Validation Statuses
+        if col == 'dbSNP_RS':
+            return self.write_dbsnp(row)
 
         if col == 'Tumor_Sample_Barcode':
             return row['tagsampler__samples']
+
+        if col == 'Consequence':
+            return self.write_consequence(row)
 
         if col == 'SYMBOL_SOURCE':
             return 'HUGO'
@@ -303,40 +392,101 @@ class Reporter(BaseReporter):
         if col == 'all_effects':
             return self.write_all_effects(row)
 
+    def write_variant_classification(self, row):
+        base_so = row['base__so']
+        variant_class = self.SO_TO_MAF_VARIANT_CLASSIFICATION
+
+        if base_so is None:
+            return ''
+
+        if base_so in variant_class:
+            return variant_class[base_so]
+        else:
+            if '_' not in base_so:
+                return base_so
+            else:
+                new_base_so = '_'.join([word.capitalize() for word in base_so.split('_')])
+                return new_base_so
+
     @staticmethod
     def write_variant_type(ref_base, alt_base):
         len_ref_base = len(ref_base)
         len_alt_base = len(alt_base)
         variant_type = ''
 
-        if len_ref_base == len_alt_base == 1 and ref_base != alt_base and ref_base != alt_base != '-':
+        if (len_ref_base == len_alt_base == 1 or len_ref_base == 1 < len_alt_base) and ref_base != alt_base != '-':
             variant_type = 'SNP'
-        elif len_ref_base == len_alt_base == 2 and ref_base != alt_base and ref_base != alt_base != '-':
+        elif (len_ref_base == len_alt_base == 2 or len_ref_base == 2 < len_alt_base) and ref_base != alt_base != '-':
             variant_type = 'DNP'
-        elif len_ref_base == len_alt_base == 3 and ref_base != alt_base and ref_base != alt_base != '-':
+        elif (len_ref_base == len_alt_base == 3 or len_ref_base == 3 < len_alt_base) and ref_base != alt_base != '-':
             variant_type = 'TNP'
-        elif len_ref_base == len_alt_base > 3 and ref_base != alt_base and ref_base != alt_base != '-':
+        elif len_ref_base > 3 and ref_base != alt_base != '-':
             variant_type = 'ONP'
-        elif (len_ref_base > len_alt_base) or (ref_base != '-' and alt_base == '-'):
+        elif ref_base != '-' and alt_base == '-':
             variant_type = 'DEL'
         elif (len_ref_base < len_alt_base) or (alt_base != '-' and ref_base == '-'):
             variant_type = 'INS'
 
         return variant_type
 
+    def write_tumor_seq_alleles(self, row, col):
+        ref_base = row['base__ref_base']
+        alt_base = row['base__alt_base']
+        variant_type = self.write_variant_type(ref_base, alt_base)
+        zygosity = ''
+        genotype = False
+
+        if ref_base == alt_base and variant_type == 'SNP':
+            zygosity = f'{ref_base}{alt_base}'
+            genotype = True
+        elif ref_base != alt_base and variant_type == 'SNP' and alt_base != '-':
+            zygosity = f'{ref_base}{alt_base}'
+            genotype = True
+
+        if col == 'Tumor_Seq_Allele1' and genotype:
+            return zygosity[1]
+        elif col == 'Tumor_Seq_Allele2' and genotype:
+            return zygosity[0]
+        elif col == 'Tumor_Seq_Allele1' and not genotype and alt_base != '-':
+            return alt_base
+        elif col == 'Tumor_Seq_Allele2' and not genotype:
+            return ''
+        else:
+            return ''
+
+    @staticmethod
+    def write_consequence(row):
+        all_mappings = row['base__all_mappings']
+
+        if not all_mappings:
+            return ''
+
+        split_collection = all_mappings.split(';')
+        split_terms = [so.split(':')[3] for so in split_collection]
+        all_split_terms = [so.split(',') if ',' in so else so for so in split_terms]
+        all_so = []
+
+        for term in all_split_terms:
+            if isinstance(term, list):
+                for _ in term:
+                    all_so.append(_)
+            else:
+                all_so.append(term)
+
+        all_so = set(all_so)
+
+        return str(';'.join(all_so))
+
     # TODO check the documentation rule here: how would I find if it is in different databases to add 'novel' or 'null'?
     @staticmethod
-    def write_dbsnp(row, col):
-        if col == 'dbSNP_RS':
-            val = row['dbsnp__rsid']
-            # reverting to null at the moment
-            if val is None:
-                return 'null'
-            else:
-                return val
+    def write_dbsnp(row):
+        val = row['dbsnp__rsid']
 
-        if col == 'dbSNP_Val_Status':
-            return ''
+        # reverting to null at the moment
+        if val is None:
+            return 'null'
+        else:
+            return val
 
     def write_all_effects(self, row):
         effects = []
