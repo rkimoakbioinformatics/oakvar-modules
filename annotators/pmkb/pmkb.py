6296ba0515adce8af6be5352befbffb13d77632d
Your Name <gehad5kamel@gmail.com>
2023-06-19 22:23:55 +0200

    updating query

--- annotators/pmkb/pmkb.py
+++ annotators/pmkb/pmkb.py
@@ -6,32 +6,36 @@ class Annotator(BaseAnnotator):
         assert input_data is not None
         if not self.cursor:
             return None
-        gene_name = input_data["Description"]
+        gene_name = input_data["hugo"]
+        a_change = input_data["achange"]
         #query interpretations database
         self.cursor.execute(
             """SELECT 
-                interpretations.Gene, "TumorType(s)", "TissueType(s)", "Variant(s)", Tier,
-                interpretations.PMKBURL, Interpretations, Citations, Description, variants.PMKBURL
-            FROM	
-	            "PMKB_Interpretations_Complete_20230526-1011" as interpretations
-            LEFT JOIN
-                "PMKB_Variants_Complete_20230526-1011" as variants ON
-                interpretations.Gene = variants.Gene
-            WHERE
-                interpretations.Gene = ?
-            """, (gene_name,)
+	                gene_name, tumor_type, tissue_type, tier, interpretations.pmkb_url,
+	                interpretations, citations, achange, variants.pmkb_url
+                FROM
+	                interpretations
+                LEFT JOIN
+	                variants as variants ON
+	                interpretations.gene_name = variants.gene
+                WHERE
+	                (CASE
+		                WHEN variants.achange IS NULL THEN interpretations.gene_name = :gene
+		                ELSE interpretations.gene_name = :gene AND variants.achange = :achange
+		            END)
+            """, {"gene": gene_name, "achange":a_change}
         )
         qr = self.cursor.fetchone()
         if qr is not None:
             return{
-              "gene_name": qr[0],  
+              "gene_name": qr[0],
               "tumor_type":qr[1],
               "tissue_type":qr[2],
               "tier": qr[3],
               "pmkb_url":qr[4],
               "interpretations": qr[5],
               "citations":qr[6],
-              "gene_description": qr[7],
+              "achange": qr[7],
               "pmkb_url": qr[8]
             }
         _ = secondary_data
