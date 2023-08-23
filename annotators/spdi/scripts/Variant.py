0a4aab7d24a056cedd2ee9523780aa03864c3bdb
Mohit Panwar <mohitsingh5123@gmail.com>
2023-08-23 11:35:54 +0200

    The new scripts folder will contain the parent class Variant and subclasses for variant types. This commit is for the Variant parent class.

---
+++ annotators/spdi/scripts/Variant.py
@@ -0,0 +1,109 @@
+"""The purpose of this module is to provide a base class for all variant types for the SPDI annotation module."""
+
+from abc import ABC, abstractmethod
+import oakvar as ov
+import importlib
+
+
+class Variant(ABC):
+    """Base class for variants."""
+
+    _wgs_reader = None
+
+    def __init__(self, chrom, pos, ref_base, alt_base):
+        self.pos = pos
+        self.chrom = chrom
+        self.ref_base = ref_base
+        self.alt_base = alt_base
+
+    @classmethod
+    @abstractmethod
+    def matches_criteria(cls, chrom, pos, ref_base, alt_base):
+        """Check if the provided data matches the criteria for this variant type."""
+
+    @classmethod
+    def classify_variant(cls, chrom, pos, ref_base, alt_base):
+        """Classify the input .crv data into a variant type by checking the matches_criteria method of each subclass."""
+        print(f"Classifying variant: chrom={chrom}, pos={pos}, ref_base={ref_base}, alt_base={alt_base}") # Print input values
+        importlib.import_module('scripts.SNP')
+        importlib.import_module('scripts.SnInsertion')
+        importlib.import_module('scripts.SnDeletion')
+        
+        for subclass in cls.__subclasses__():
+            print(f"Checking subclass: {subclass.__name__}") # Print subclass name
+            match = subclass.matches_criteria(chrom, pos, ref_base, alt_base)
+            print(f"Result of matches_criteria for {subclass.__name__}: {match}") # Print result of matches_criteria
+
+            if match:
+                return subclass(chrom, pos, ref_base, alt_base)
+
+        return UnclassifiedVariant(chrom, pos, ref_base, alt_base)
+
+    @abstractmethod
+    def to_spdi(self):
+        """Converts the variant to SPDI format."""
+
+    @abstractmethod
+    def _left_align(self):
+        """Helper method for left alignment."""
+
+    @abstractmethod
+    def _get_left_context(self):
+        """Abstract method to capture the repeated sequence to the left."""
+
+    @abstractmethod
+    def _get_right_context(self):
+        """Abstract method to capture the repeated sequence to the right."""
+
+    @abstractmethod
+    def _construct_contextual_allele(self):
+        """Helper method for constructing contextual allele."""
+
+    @classmethod
+    def set_wgs_reader(cls, reader):
+        """Set the WGS reader for all instances of this class."""
+        cls._wgs_reader = reader
+
+    @classmethod
+    def _get_base(cls, chrom, pos):
+        """Get a single base from the WGS."""
+        return cls._wgs_reader.get_bases(chrom, pos)
+
+    @classmethod
+    def _get_bases(cls, chrom, start, end):
+        """Get a range of bases from the WGS."""
+        return cls._wgs_reader.get_bases(chrom, start, end=end)
+
+    @staticmethod
+    # Initialize the wgs_reader once for the entire module
+    def initialize_wgs_reader(assembly="hg38"):
+        """
+        Initialize the WGS reader for the module.
+        """
+        wgs_reader = ov.get_wgs_reader(assembly)
+        Variant.set_wgs_reader(wgs_reader)
+
+
+class UnclassifiedVariant(Variant):
+    """
+    The purpose of this class is to populate all the cells of input data that are unclassified.
+    """
+
+    def _construct_contextual_allele(self):
+        pass
+
+    def _get_left_context(self):
+        pass
+
+    def _get_right_context(self):
+        pass
+
+    def _left_align(self):
+        pass
+
+    @classmethod
+    def matches_criteria(cls, chrom, pos, ref_base, alt_base):
+        pass
+
+    def to_spdi(self):
+        return "Unable to classify the given variant data."
