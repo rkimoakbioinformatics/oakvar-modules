4bf146bb8bb4aabb883522d5ca684a7109fb1e99
Your Name <gehad5kamel@gmail.com>
2023-09-08 19:49:54 +0200

    follow-up cases

--- annotators/pmkb/pmkb.py
+++ annotators/pmkb/pmkb.py
@@ -10,8 +10,9 @@ class Annotator(BaseAnnotator):
         gene = input_data['hugo']
         transcript = input_data['transcript']
         achange = input_data['achange'].split('.')[0]
+        exonno = input_data['exonno']
         #Handle missense variants
-        if 'missense' in input_data['all_mappings'].split(":")[3]:
+        if 'missense' in input_data['all_mappings'].split(':')[3]:
             input_ref_alt_pos = re.search(r'(\w{3})(\d+)(\w{3})',achange)
             ref_alt_pos_catch = input_ref_alt_pos.groups()
             input_pos = ref_alt_pos_catch[1]
@@ -24,35 +25,79 @@ class Annotator(BaseAnnotator):
                 FROM 
                     variant
                 WHERE 
-                    Gene = :gene AND transcript_id = :transcript 
+                    Gene = :gene AND transcript_id = :transcript
             """, {"gene": gene, "transcript": transcript})
             #save results in pmkb_variants variable    
             pmkb_variants = self.cursor.fetchall()
             #loop through results to get a match
             for line in pmkb_variants:
                 pmkb_achange = line[0]
-                if len(pmkb_achange.split(':')> 5):
-                    #get pos:ref_allele_alt_allele
-                    pmkb_pos = achange_pmkb.split(':')[1]
-                    pmkb_ref_allele = achange_pmkb.split(':')[4]
-                    pmkb_alt_allele = achange_pmkb.split(':')[5]
-                    #match results of pmkb achange with input data
-                    if pmkb_pos == input_pos and pmkb_ref_allele == input_ref_allele and pmkb_alt_allele == input_alt_allele:
-                        self.cursor.execute("""
-                            SELECT 
-                                * 
-                            FROM 
-                                variant
-            
-                            LEFT JOIN 
-                                interpretations_final
-                                ON variant.achange = interpretations_final.variants
-                            WHERE 
-                                achange = :achange_pmkb
-                        """, {"achange_pmkb": achange_pmkb})
-                        qr = self.cursor.fetchall()
+                #for exon cases 
+                #             
+                #get pos:ref_allele_alt_allele
+                pmkb_pos = achange_pmkb.split(':')[1]
+                pmkb_ref_allele = achange_pmkb.split(':')[-2]
+                pmkb_alt_allele = achange_pmkb.split(':')[-1]
+                #match results of pmkb achange with input data
+                if pmkb_pos == input_pos and pmkb_ref_allele == input_ref_allele and pmkb_alt_allele == input_alt_allele:
+                    self.cursor.execute("""
+                        SELECT 
+                            gene_name, tumor_type,tissue_type,tier,variant.pmkb_url,interpretations,
+                            citations,achange, interpretations_fina.pmkb_url
+                        FROM 
+                            variant
+                        LEFT JOIN 
+                            interpretations_final
+                            ON variant.achange = interpretations_final.variants
+                        WHERE 
+                            achange = :achange_pmkb
+                    """, {"achange_pmkb": achange_pmkb})
+                    qr = self.cursor.fetchall()
+        #handle frameshift mutations
+        elif 'frameshift' in input_date['all_mappings'].split(':')[3]:
+            #get input data for querying the pmkb database
+            input_ref_alt_pos = re.search(r'(\w{3})(\d+)',achange)
+            ref_alt_pos_catch = input_ref_alt_pos.groups()
+            input_pos = ref_alt_pos_catch[1]
+            input_ref_allele = seq1(ref_alt_pos_catch[0])
 
-        # qr = self.cursor.fetchone()
+            #query the pmkb database
+            self.cursor.execute("""
+                SELECT 
+                    achange
+                FROM 
+                    varian
+                WHERE Gene = :gene AND transcript_id = :transcript
+            """, {"gene": gene, "transcript": transcript})
+            #fetch the achanges
+            pmkb_variants = self.cursor.fetchall()
+            #get pmkb pos:ref_allele:alt_allele
+            for line in pmkb_variants:
+                #get pos:ref_allele_alt_allele
+                pmkb_pos = achange_pmkb.split(':')[1]
+                pmkb_ref_allele = achange_pmkb.split(':')[-2]
+                if pmkb_pos == input_pos and pmkb_ref_allele == input_ref_allele:
+                    self.cursor.execute("""
+                        SELECT 
+                            gene_name, tumor_type,tissue_type,tier,variant.pmkb_url,interpretations,
+                            citations,achange, interpretations_fina.pmkb_url
+                        FROM 
+                            variant
+                        LEFT JOIN 
+                            interpretations_final
+                            ON variant.achange = interpretations_final.variants
+                        WHERE 
+                            achange = :achange_pmkb""", {"achange_pmkb": achange_pmkb})
+                    qr = self.cursor.fetchall()
+        elif 'insertion' in input_date['all_mappings'].split(':')[3]:
+            pass    
+        elif 'indel' in input_data['all_mappings'].split(':')[3]:
+            pass
+        elif 'deletion' in input_data['all_mappings'].split(':')[3]:
+            pass
+        
+        #handle frameshift mutation
+
         if qr is not None:
             return{
               "gene_name": qr[0],
