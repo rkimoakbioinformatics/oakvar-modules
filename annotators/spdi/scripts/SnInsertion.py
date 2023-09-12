e578162c2c1f902ce802ee4d0c6064a00dd7c625
Mohit Panwar <mohitsingh5123@gmail.com>
2023-09-12 19:13:56 +0200

    use 'to_upper = True' instead of .upper()

--- annotators/spdi/scripts/SnInsertion.py
+++ annotators/spdi/scripts/SnInsertion.py
@@ -17,14 +17,14 @@ class SnInsertion(Variant):
     @classmethod
     def matches_criteria(cls, chrom, pos, ref_base, alt_base):
         # Logic to determine if it's an SnInsertion.
-        return ref_base == "-" and len(alt_base) == 1
+        return ref_base == '-' and len(alt_base) == 1
 
     def _get_left_context(self):
-        left_context = ""
+        left_context = ''
         left_pos = self.pos - 1
 
         while True:
-            base_at_pos = self._wgs_reader.get_bases(self.chrom, left_pos)
+            base_at_pos = self._wgs_reader.get_bases(self.chrom, left_pos, to_upper=True)
             # Update the left_context if the base matches the last character of alt_base
             if base_at_pos == self.alt_base[-1]:
                 left_context = base_at_pos + left_context
@@ -36,31 +36,30 @@ class SnInsertion(Variant):
 
     def _get_right_context(self):
         """Retrieve the repeated sequence to the right of the insertion."""
-        right_context = ""
+        right_context = ''
         right_pos = self.pos + 1  # Start from the position next to the insertion
 
-        base_at_insertion_pos = self._wgs_reader.get_bases(self.chrom, self.pos)
+        base_at_insertion_pos = self._wgs_reader.get_bases(self.chrom, self.pos, to_upper=True)
         if base_at_insertion_pos != self.alt_base[-1]:  # If the base at insertion position doesn't match the inserted base
             return right_context  # Return empty context
 
         while True:
-            base_at_pos = self._wgs_reader.get_bases(self.chrom, right_pos)
+            base_at_pos = self._wgs_reader.get_bases(self.chrom, right_pos, to_upper=True)
             if base_at_pos == self.alt_base[-1]:
                 # If the base at the insertion position matches and it's the first time in the loop
-                if right_context == "":
+                if not right_context:
                     right_context += base_at_insertion_pos
                 right_context += base_at_pos
                 right_pos += 1
             else:
                 break
-
+            
         return right_context
-
     
     def _construct_contextual_allele(self):
         """Construct the reference and alternate alleles with context."""
         if not self.left_context and not self.right_context:
-            self.ref_base = "-"
+            self.ref_base = '-'
             self.alt_base = self.alt_base[-1]
         elif self.left_context and not self.right_context:
             self.ref_base = self.left_context
@@ -72,20 +71,11 @@ class SnInsertion(Variant):
             self.ref_base = self.left_context + self.right_context
             self.alt_base = self.left_context + self.alt_base[-1] + self.right_context
             
-        print(f"Left Context: {self.left_context}")
-        print(f"Right Context: {self.right_context}")
-        print(f"Ref Base: {self.ref_base}")
-        print(f"Alt Base: {self.alt_base}")
-
-
     def _left_align(self):
-        shift = len(self.left_context)  # This determines how many bases we can shift to the left
+        shift = len(self.left_context)
         if shift > 0:
             self.pos -= shift
-        print(len(self.left_context))
-
-
 
     def to_spdi(self):
         adjusted_pos = self.pos - 1  # Convert from 1-based to 0-based indexing
-        return f"{self.chrom}:{adjusted_pos}:{self.ref_base}:{self.alt_base}"
+        return f'{self.chrom}:{adjusted_pos}:{self.ref_base}:{self.alt_base}'
--- annotators/spdi/tests/SnInsertion_test.py
+++ annotators/spdi/tests/SnInsertion_test.py
@@ -20,28 +20,7 @@ class TestSnInsertion:
     
     def test_matches_criteria(self, chrom, position, ref_base, alt_base, is_valid_sninsertion, mock_data, expected_spdi):
         assert SnInsertion.matches_criteria(chrom, position, ref_base, alt_base) == is_valid_sninsertion
-
-    @pytest.mark.parametrize(
-        "initial_position, mock_data, expected_position_after_left_align",
-        [
-            (1000, {999: "G", 1000: "A", 998: "G", 997: "G"}, 997),
-            (1000, {999: "G", 1000: "A", 998: "G", 997: "A"}, 998),
-           
-        ]
-    )
-    def test_left_align(self, initial_position, mock_data, expected_position_after_left_align, mocked_wgs_reader):
-        
-        def mock_get_base(chrom, pos):
-            return mock_data.get(pos, "A")
-                
-        mocked_wgs_reader.get_bases.side_effect = mock_get_base
-        
-        SnInsertion._wgs_reader = mocked_wgs_reader
-        sninsertion = SnInsertion("chr1", initial_position, "-", "G")
-        assert sninsertion.pos == expected_position_after_left_align
-        
-        
-        
+    
     @pytest.mark.parametrize(
     "initial_position, alt_base, mock_data, expected_left_context",
     [
@@ -52,7 +31,7 @@ class TestSnInsertion:
 )
     def test_get_left_context(self, initial_position, alt_base, mock_data, expected_left_context, mocked_wgs_reader):
             
-        def mock_get_base(chrom, pos, data=mock_data):  # Bind mock_data to mock_get_base
+        def mock_get_base(chrom, pos, to_upper = True, data=mock_data):  # Bind mock_data to mock_get_base
             if pos not in data:
                 print(f"Position {pos} not found in mock data!")
                 print(f"Current mock data: {data}")
@@ -76,7 +55,7 @@ class TestSnInsertion:
 )
     def test_get_right_context(self, initial_position, alt_base, mock_data, expected_right_context, mocked_wgs_reader):
 
-        def mock_get_base(chrom, pos, data=mock_data):  # Bind mock_data to mock_get_base
+        def mock_get_base(chrom, pos, to_upper = True, data=mock_data):  # Bind mock_data to mock_get_base
             if pos not in data:
                 print(f"Position {pos} not found in mock data!")
                 print(f"Current mock data: {data}")
@@ -108,7 +87,7 @@ class TestSnInsertion:
     def test_construct_contextual_allele(self, initial_position, alt_base, mock_data, expected_ref_base, expected_alt_base, mocked_wgs_reader):
         
         # Mocking method
-        def mock_get_base(chrom, pos, data=mock_data):
+        def mock_get_base(chrom, pos, to_upper = True, data=mock_data):
             return data.get(pos)
         
         mocked_wgs_reader.get_bases.side_effect = mock_get_base
@@ -117,6 +96,23 @@ class TestSnInsertion:
         sninsertion = SnInsertion("chr1", initial_position, "-", alt_base)
         assert sninsertion.ref_base == expected_ref_base
         assert sninsertion.alt_base == expected_alt_base
+
+    @pytest.mark.parametrize(
+    "chrom, pos, ref_base, alt_base, mock_data, expected_position_after_left_align",
+    [
+        ("chr1", 1000, "-", "G", {999: "G", 1000: "A", 998: "G", 997: "G"}, 997),
+        ("chr1", 1000, "-", "G", {999: "G", 1000: "A", 998: "G", 997: "A"}, 998),
+    ]
+)
+    def test_left_align(self, chrom, pos, ref_base, alt_base, mock_data, expected_position_after_left_align, mocked_wgs_reader):
+        
+        def mock_get_base(chrom, pos, to_upper=True, data=mock_data):
+            return mock_data.get(pos, "A")
+                
+        mocked_wgs_reader.get_bases.side_effect = mock_get_base
+        SnInsertion._wgs_reader = mocked_wgs_reader
+        sninsertion = SnInsertion(chrom, pos, ref_base, alt_base)
+        assert sninsertion.pos == expected_position_after_left_align
 
     @pytest.mark.parametrize(
     "chrom, pos, ref_base, alt_base, mock_data, expected_spdi",
@@ -137,8 +133,8 @@ class TestSnInsertion:
 
     def test_to_spdi(self, chrom, pos, ref_base, alt_base, mock_data, expected_spdi, mocked_wgs_reader):
         # Mocking method
-        def mock_get_base(chrom, position, data=mock_data):
-            return data.get(position)
+        def mock_get_base(chrom, pos, to_upper = True, data=mock_data):
+            return data.get(pos)
 
         # Set up the mock
         mocked_wgs_reader.get_bases.side_effect = mock_get_base
