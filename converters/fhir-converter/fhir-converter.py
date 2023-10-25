33c421d5dc69d72600d7f580441de28f0d69357d
AkRampersad <akashjrampersad@gmail.com>
2023-10-25 15:52:01 -0400

    Chromosome conversion and position
    Chromosome conversion and position information are now acquired and converted properly.
    
    If there is no chromosome present in txt file the accession number is used for now.

--- converters/fhir-converter/fhir-converter.py
+++ converters/fhir-converter/fhir-converter.py
@@ -11,27 +11,20 @@ import requests
 import hashlib
 import sqlite3
 from oakvar import BaseConverter
-from fhir.resources.patient import Patient
-from fhir.resources.observation import Observation
-from fhir.resources.observation import ObservationComponent
-from fhir.resources.humanname import HumanName
-from fhir.resources.codeableconcept import CodeableConcept
-from fhir.resources.coding import Coding
-from fhir.resources.reference import Reference
-from fhir.resources.bundle import Bundle, BundleEntry
-from fhir.resources.fhirtypes import Uri, MetaType, IdentifierType, String, RangeType
-from fhir.resources.quantity import Quantity
-from fhir.resources.identifier import Identifier
+
+
 
 
 class Converter(BaseConverter):
     def __init__(self, input_path, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.format_name = "FHIR"
+        self.format_name = 'FHIR' or 'fhir'
         self.resources = None
         self.filename = pathlib.Path(input_path)
+     
 
-    def check_format(self, input_path:str) -> bool:
+
+    def check_format(self,input_path, *__args__, **__kwargs__)-> bool:
         """
         Detect the format of an input file.
 
@@ -45,7 +38,6 @@ class Converter(BaseConverter):
         VCF file format.
         """
         filename = pathlib.Path(input_path)
-        FHIR_validate = False
         with open(filename, 'r') as fn:
             parser = ijson.parse(fn)
             state = None
@@ -53,73 +45,137 @@ class Converter(BaseConverter):
                 if event == 'map_key': 
                     state = value 
                     if state == 'resourceType':
-                        FHIR_validate = True
-        return FHIR_validate
+                        return True 
 
 
-    # If your converter module needs something else than
-    # the standard way of opening a text input file,
-    # read line by line, and coverting each line into
-    # a list of dictionaries of variants,
-    # you may want to start with modifying
-    # convert_file method. In that case, uncomment
-    # the below convert_file method and add your implementation.
-    #
-    def convert_file(self, *args, **kwargs):
-        fhir_data = {}
-        with open(self.filename, 'r', encodng='utf-8') as json_file:
-            parser = ijson.parse(json_file)
-            for prefix,event,value in parser:
-                if event == 'map_key':
-                    current_key = value 
-                elif event =='string' or event == 'number':
-                    fhir_data[current_key] = value
-                elif event == 'end_map':
-                    pass
-        return fhir_data
-    #     self, file, *__args__, exc_handler=None, **__kwargs__
-    # ) -> Iterator[Tuple[int, List[dict]]]:
-    #     line_no = 0
-    #     for line in file:
-    #         line_no += 1
-    #         try:
-    #             yield line_no, self.convert_line(line)
-    #         except Exception as e:
-    #             if exc_handler:
-    #                 exc_handler(line_no, e)
-    #             else:
-    #                 raise e
-    #     return None
+    def refseq_to_USCS(self,chromosome_mappings):
+        refseq_2_uscs_dict = {}
+        with open(chromosome_mappings,'r') as file:
+            for line in file: 
+                key, value  = line.split('c')
+                updated_key = key.split(".")[0]
+                refseq_2_uscs_dict[updated_key.strip()] = value.strip()
 
-    def convert_line(self, line) -> List[Dict]:
-        """
-        Converts a line from an input file to OakVar's variant dict.
+        for item in refseq_2_uscs_dict.keys():
+            refseq_2_uscs_dict[item] = "c" + refseq_2_uscs_dict[item]
 
-        Arguments:
-            l: a string of a line from an input file
-        Returns:
-            dict: a list of dicts, each dict for a variant collected
-                  from the input line. Each dict should have
-                  the following required fields:
+        return refseq_2_uscs_dict
+    def chrom_finder(self,db,transcript,dict) -> str: 
+        #trim transcript if not in correct format
+        str_tr = transcript.split(':')[0]
 
-                  chrom: chromosome name [str]
-                  pos: chromosomal position [int]
-                  ref_base: reference bases [str]
-                  alt_base: altername bases [str]
+        final_str = str_tr.split(".")[0]
 
-                  Optional fields for each dict are:
+        if final_str[0] != "N":
+            conn = sqlite3.connect(db)
+            curs = conn.cursor()
 
-                  sample_id: the ID or name of a sample having the variant [list[str]]
-                  tags: a custom tag given to the variant [list[str]]
-        """
-        _ = line
+            curs.execute(f'SELECT "chromid" from "transcript" WHERE "name" = "{str_tr}"')
+
+
+            chrom_id = curs.fetchall()[0][0]
+            #print(chrom_id)
+
+            curs.execute(f'SELECT "chrom" from "chroms" WHERE "chromid" = {chrom_id}')
+            chrom = curs.fetchall()[0][0]
+
+            conn.close()
+            return chrom
+        else: 
+            return dict[final_str]
+
+
+
+    def convert_file(self, file):
+        chr_dict = self.refseq_to_USCS("GRCh38_RefSeq2UCSC.txt")
+        gencode_db = ""
+        start_terms = ['resourceType','name','resource','variant']
         var_dicts = []
         var_dict = {
-            "chrom": "chr1",
-            "pos": 2878349,
-            "ref_base": "A",
-            "alt_base": "T",
-            "sample_id": "sample1",
+            "chrom": None ,
+            "pos":  None,
+            "ref_base": None ,
+            "alt_base": None,
+            "sample_id": None,
         }
-        var_dicts.append(var_dict)
-        return var_dicts
+        ref_or_alt = None 
+
+
+        allele_codes = ["69547-8","69551-0"]
+        HGVS_codes = ["48013-7","51958-7",'48004-6']
+        pos_codes = ["81254-5"]
+
+
+        in_hgvs_comp = False
+        in_pos_comp = False
+
+        hgvs_systems = ["http://varnomen.hgvs.org",'https://www.ncbi.nlm.nih.gov/dbvar/','https://api.ncbi.nlm.nih.gov/variation/v0/',
+                        "http://www.ensembl.org"]
+
+
+
+
+        with open(file, 'r') as json_file:
+            parser = ijson.parse(json_file)
+            for prefix,event,value in parser:
+
+
+                #check to see if in
+                if event == 'map_key':
+                    current_key = value
+                    if current_key in start_terms:
+                            var_dict = {
+                                "chrom": None ,
+                                "pos":  None,
+                                "ref_base": None ,
+                                "alt_base": None,
+                                "sample_id": None,
+                            }
+
+                #look at component value
+                elif event =='string' or event == 'number':
+
+                    #check to see if in hgvs system
+                    if current_key == 'system':
+                        if value in hgvs_systems:
+                            #print(value)
+                            in_hgvs_comp = True
+
+                    # check to see if in a position component to extract pos 
+                    if in_pos_comp and current_key == 'value':
+                        in_pos_comp = False
+                        var_dict['pos'] = value
+
+                    if current_key == 'code':
+
+                        if in_hgvs_comp:
+                            #print(value)
+                            in_hgvs_comp = False
+                            try:
+                                var_dict['chrom'] = self.chrom_finder(gencode_db,value,chr_dict)
+                            except: var_dict['chrom'] = value
+
+                        if value in HGVS_codes:
+                            in_hgvs_comp = True
+                        if value in pos_codes:
+                            in_pos_comp = True 
+
+                        if value == "69547-8":
+                            ref_or_alt = 'Ref'
+                        if value == "69551-0":
+                            ref_or_alt = 'Alt'
+
+                    if current_key == 'valueString' and ref_or_alt == "Ref":
+                        var_dict['ref_base'] = value
+
+                    if current_key == 'valueString' and ref_or_alt == 'Alt':
+
+                        var_dict['alt_base'] = value
+
+                        var_dicts.append(var_dict)
+
+
+
+                elif event == 'end_map':
+                    pass
+            return var_dicts
