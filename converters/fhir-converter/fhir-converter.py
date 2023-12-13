5af7a0e6e15022da41339d0e11491644b3898eea
Akash Rampersad <93301335+AkRampersad@users.noreply.github.com>
2023-12-13 13:29:30 -0500

    FHIR-converter MVP
    This is push has a working FHIR-converter module.
    
    If a FHIR doc is converted into a OakVar sqlite then back into a FHIR document the given and produced FHIR files are the same. The only differences are the UUIDs. 
    
    summation: FHIR files produced and FHIR files read are the same

--- converters/fhir-converter/fhir-converter.py
+++ converters/fhir-converter/fhir-converter.py
@@ -1,4 +1,5 @@
 import os
+
 import pathlib
 import ijson
 import glob
@@ -10,11 +11,12 @@ import oakvar as ov
 class Converter(BaseConverter):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.format_name = 'FHIR' or 'fhir'
+        self.format_name = "FHIR" or "fhir"
         self.resources = None
         self.filename = pathlib.Path(self.input_path)
         self.total_lines = None
         self.line_no = 0
+        self.lines = None
 
     def setup(self, *args, **kwargs):
         fhir_dir = ov.lib.module.local.get_module_dir("fhir-converter")
@@ -42,25 +44,28 @@ class Converter(BaseConverter):
 
         self.chr_dict = self.refseq_to_USCS(mapping_file)
 
-        self.start_terms = ['resourceType', 'name', 'resource', 'variant']
+        self.start_terms = ["resourceType", "name", "resource", "variant"]
         self.var_dict = {
             "chrom": None,
-            "pos":  None,
+            "pos": None,
             "ref_base": None,
             "alt_base": None,
             "sample_id": None,
         }
         self.ref_or_alt = None
         self.allele_codes = ["69547-8", "69551-0"]
-        self.HGVS_codes = ["48013-7", "51958-7", '48004-6']
+        self.HGVS_codes = ["48013-7", "51958-7", "48004-6"]
         self.pos_codes = ["81254-5"]
         self.in_hgvs_comp = False
         self.in_pos_comp = False
-        self.hgvs_systems = ["http://varnomen.hgvs.org",
-                             'https://www.ncbi.nlm.nih.gov/dbvar/',
-                             'https://api.ncbi.nlm.nih.gov/variation/v0/',
-                             'http://www.ensembl.org']
-        json_file = open(self.input_path, 'r')
+        self.in_name_comp = False
+        self.hgvs_systems = [
+            "http://varnomen.hgvs.org",
+            "https://www.ncbi.nlm.nih.gov/dbvar/",
+            "https://api.ncbi.nlm.nih.gov/variation/v0/",
+            "http://www.ensembl.org",
+        ]
+        json_file = open(self.input_path, "r")
         self.parser = ijson.parse(json_file)
 
     def check_format(self, input_path, *__args__, **__kwargs__) -> bool:
@@ -77,28 +82,20 @@ class Converter(BaseConverter):
         VCF file format.
         """
         filename = pathlib.Path(input_path)
-        with open(filename, 'r') as fn:
+        with open(filename, "r") as fn:
             parser = ijson.parse(fn)
             state = None
             for prefix, event, value in parser:
-                if event == 'map_key':
+                if event == "map_key":
                     state = value
-                    if state == 'resourceType':
+                    if state == "resourceType":
                         return True
-
-    # If your converter module needs something else than
-    # the standard way of opening a text input file,
-    # read line by line, and coverting each line into
-    # a list of dictionaries of variants,
-    # you may want to start with modifying
-    # convert_file method. In that case, uncomment
-    # the below convert_file method and add your implementation.
 
     def refseq_to_USCS(self, chromosome_mappings):
         refseq_2_uscs_dict = {}
-        with open(chromosome_mappings, 'r') as file:
+        with open(chromosome_mappings, "r") as file:
             for line in file:
-                key, value = line.split('c')
+                key, value = line.split("c")
                 updated_key = key.split(".")[0]
                 update_value = value.split("_")[0]
                 refseq_2_uscs_dict[updated_key.strip()] = update_value.strip()
@@ -110,7 +107,7 @@ class Converter(BaseConverter):
 
     def chrom_finder(self, db, transcript, dict) -> str:
         # trim transcript if not in correct format
-        str_tr = transcript.split(':')[0]
+        str_tr = transcript.split(":")[0]
 
         final_str = str_tr.split(".")[0]
 
@@ -118,13 +115,11 @@ class Converter(BaseConverter):
             conn = sqlite3.connect(db)
             curs = conn.cursor()
 
-            curs.execute(
-                f'SELECT "chromid" from "transcript" WHERE "name" ="{str_tr}"')
+            curs.execute(f'SELECT "chromid" from "transcript" WHERE "name" ="{str_tr}"')
 
             chrom_id = curs.fetchall()[0][0]
 
-            curs.execute(
-                f'SELECT "chrom" from "chroms" WHERE "chromid" ={chrom_id}')
+            curs.execute(f'SELECT "chrom" from "chroms" WHERE "chromid" ={chrom_id}')
             chrom = curs.fetchall()[0][0]
 
             conn.close()
@@ -137,7 +132,7 @@ class Converter(BaseConverter):
 
     def dict_checker(self, dict):
         for key in dict:
-            if key != 'sample_id' and dict[key] is None:
+            if key != "sample_id" and dict[key] is None:
                 return False
         return True
 
@@ -148,37 +143,36 @@ class Converter(BaseConverter):
             if self.chrom_finder(gen_db, value, chrms) is None:
                 self.in_hgvs_comp = False
             try:
-                self.var_dict['chrom'] = self.chrom_finder(
-                    gen_db, value, chrms)
+                self.var_dict["chrom"] = self.chrom_finder(gen_db, value, chrms)
             except:
-                self.var_dict['chrom'] = value
+                self.var_dict["chrom"] = value
         if value in self.HGVS_codes:
             self.in_hgvs_comp = True
         if value in self.pos_codes:
             self.in_pos_comp = True
         if value == "69547-8":
-            self.ref_or_alt = 'Ref'
+            self.ref_or_alt = "Ref"
         if value == "69551-0":
-            self.ref_or_alt = 'Alt'
+            self.ref_or_alt = "Alt"
 
     def check_valueString(self, prefix, event, value):
-        if self.ref_or_alt == 'Ref':
-            self.var_dict['ref_base'] = value
-        if self.ref_or_alt == 'Alt':
-            self.var_dict['alt_base'] = value
+        if self.ref_or_alt == "Ref":
+            self.var_dict["ref_base"] = value
+        if self.ref_or_alt == "Alt":
+            self.var_dict["alt_base"] = value
 
     def make_total_lines(self, input_path, num_pool, batch_size):
         lines = {}
         core_num = 0
         temp_lines = []
         line_no = 0
-        component = []
         immature_exit = True
-        with open(input_path, 'r') as json_file:
+        sample_name = None
+        with open(input_path, "r") as json_file:
             parser = ijson.parse(json_file)
             for prefix, event, value in parser:
                 if self.dict_checker(self.var_dict):
-                    # print(line_no, self.var_dict)
+                    self.var_dict["sample_id"] = sample_name
                     temp_lines.append((line_no, self.var_dict))
                     line_no += 1
                     if len(temp_lines) == batch_size:
@@ -188,56 +182,59 @@ class Converter(BaseConverter):
 
                     self.var_dict = {
                         "chrom": None,
-                        "pos":  None,
+                        "pos": None,
                         "ref_base": None,
                         "alt_base": None,
-                        "sample_id": None,
+                        "sample_id": sample_name,
                     }
-
-                # check to see if in
-                if event == 'map_key':
+                if event == "map_key":
                     current_key = value
-                # look at component value
-                elif event == 'string' or event == 'number':
+                    if value == "name":
+                        self.in_name_comp = True
+                elif event == "string" or event == "number":
+                    if self.in_name_comp is True and current_key == "given":
+                        sample_name = value
                     if self.in_pos_comp is True:
-                        if len(component) > 2:
-                            self.var_dict["pos"] = int(component[1]['value'])
-                            component = []
+                        if type(value) is int:
+                            self.var_dict["pos"] = value
                             self.in_pos_comp = False
-                        else:
-                            component.append({current_key: value})
 
                     # check to see if in hgvs system
-                    if current_key == 'system':
+                    if current_key == "system":
                         if value in self.hgvs_systems:
                             self.in_hgvs_comp = True
-
+                    if current_key == "name":
+                        self.in_name_comp = True
                     # check to see if in a position component to extract pos
-                    if self.in_pos_comp and current_key == 'value':
+                    if self.in_pos_comp and current_key == "value":
                         self.in_pos_comp = False
-                        self.var_dict['pos'] = value
+                        self.var_dict["pos"] = value
 
-                    if current_key == 'code':
+                    if current_key == "code":
                         self.check_parsed_code(prefix, event, value)
 
-                    if current_key == 'valueString':
+                    if current_key == "valueString":
                         self.check_valueString(prefix, event, value)
 
-                elif event == 'end_map':
+                elif event == "end_map":
                     pass
         self.total_lines = temp_lines
-        return temp_lines, immature_exit
+        return self.total_lines, immature_exit
 
     def get_variant_lines(self, input_path, num_pool, start_line_no, batch_size):
         immature_exit = True
         if self.total_lines is None:
             self.make_total_lines(input_path, num_pool, batch_size)
-            lines = {core_num: [] for core_num in range(num_pool)}
+            self.lines = {core_num: [] for core_num in range(num_pool)}
             for core_num in range(num_pool):
                 start = self.line_no + batch_size * core_num
-                end = min(start + batch_size, len(self.total_lines)+1)
-                lines[core_num] = self.total_lines[start:end]
-            return lines, immature_exit
+                end = min(start + batch_size, len(self.total_lines) + 1)
+                self.lines[core_num] = self.total_lines[start:end]
+            immature_exit = False
+            return self.lines, immature_exit
+        else:
+            immature_exit = False
+            return self.lines, immature_exit
 
     def convert_line(self, line):
-        return line
+        return [line]
