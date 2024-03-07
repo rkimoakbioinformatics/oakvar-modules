7343faee07b18ca7afab4c731e759849521427fa
Akash Rampersad <93301335+AkRampersad@users.noreply.github.com>
2024-03-07 00:16:16 -0500

    implemented module_options
    prototype implementation of module_options as well as introducing functions for adding gnomad3 allele frequency

---
+++ reporters/fhirreporter/fhir_component.py
@@ -0,0 +1,111 @@
+# OakVar Dual License
+# 
+# Copyright (c) 2023 Oak Bioinformatics, LLC
+# 
+# This program is dual licensed under the Affero GPL-3.0 or later for 
+# non-commercial and open source use, and under a commercial license, 
+# which is available for purchase, for closed-source or commercial use.
+# 
+# For the commercial use, please contact Oak Bioinformatics, LLC 
+# for obtaining such a license. OakVar commercial license does not impose 
+# the Affero GPL open-source licensing terms, conditions, and limitations. 
+# To obtain a commercial-use license of OakVar, please visit our website at
+# https://oakbioinformatics.com or contact us at info@oakbioinformatics.com 
+# for more information.
+# 
+
+from fhir.resources.fhirtypes import Uri
+from fhir.resources.coding import Coding
+from fhir.resources.codeableconcept import CodeableConcept
+from fhir.resources.observation import ObservationComponent
+
+def get_base_component_gene():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "48018-6"   # type: ignore
+    code_coding.display = "Gene studied [ID]" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_achange_hgvs():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "48005-3" # type: ignore
+    code_coding.display = "Amino acid change (pHGVS)" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_cchange_hgvs():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "48004-6" # type: ignore
+    code_coding.display = "DNA change (c.HGVS)" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_transcript():
+    code_coding = Coding() # type: ignore
+    code_coding.system = "http://loinc.org" # type: ignore
+    code_coding.code = "51958-7" # type: ignore
+    code_coding.display = "Transcript reference sequence [ID]" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_start_stop():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "81254-5" # type: ignore
+    code_coding.display = "Genomic allele start-end" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_ref_base():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "69547-8"  # type: ignore # always code for reference allele
+    code_coding.display = "Ref nucleotide" # type: ignore
+    code = CodeableConcept() # type: ignore
+    code.coding = [code_coding] # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_alt_base():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "69551-0" # type: ignore
+    code_coding.display = "Alt allele" # type: ignore
+    code = CodeableConcept() # type: ignore
+    code.coding = [code_coding] # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_chrom():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "48000-4" # type: ignore
+    code = CodeableConcept() # type: ignore
+    code.coding = [code_coding] # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_coord_system():
+    code_coding = Coding() # type: ignore
+    code_coding.system = Uri("http://loinc.org")
+    code_coding.code = "92822-6" # type: ignore
+    code = CodeableConcept() # type: ignore
+    code.coding = [code_coding] # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_base_component_feature_consequence():
+    code_coding = Coding() # type: ignore
+    code_coding.system = "http://hl7.org/fhir/uv/genomics-reporting/STU2/CodeSystem-tbd-codes-cs" # type: ignore
+    code_coding.code = "feature-consequence" # type: ignore
+    code = CodeableConcept(coding=[code_coding]) # type: ignore
+    return ObservationComponent(code=code) # type: ignore
+
+def get_gnomad_allele_frequency():
+    code_coding = Coding # type : ignore
+    code_coding.system = Uri("http://loinc.org") # type: ignore
+    code_coding.code = "92821-8"# type : ignorore
+    code_coding.display = "gnomAD v3 Allele Frequency"# type: ignore
+    code = CodeableConcept()# type: ignore
+    code.coding = [code_coding]# type: ignore
+    return ObservationComponent(code=code)# type:ignore
+
---
+++ reporters/fhirreporter/fhir_consts.py
@@ -0,0 +1,109 @@
+# OakVar Dual License
+# 
+# Copyright (c) 2023 Oak Bioinformatics, LLC
+# 
+# This program is dual licensed under the Affero GPL-3.0 or later for 
+# non-commercial and open source use, and under a commercial license, 
+# which is available for purchase, for closed-source or commercial use.
+# 
+# For the commercial use, please contact Oak Bioinformatics, LLC 
+# for obtaining such a license. OakVar commercial license does not impose 
+# the Affero GPL open-source licensing terms, conditions, and limitations. 
+# To obtain a commercial-use license of OakVar, please visit our website at
+# https://oakbioinformatics.com or contact us at info@oakbioinformatics.com 
+# for more information.
+# 
+
+so_term_to_id_dict = {
+    "start_lost": "SO:0002012",
+    "intron_variant": "SO:0001627",
+    "frameshift_elongation": "SO:0001909",
+    "missense_variant": "SO:0001583",
+    "stop_lost": "SO:0001578",
+    "frameshift_truncation": "SO:0001910",
+    "lnc_RNA": "SO:0002127",
+    "inframe_insertion": "SO:0001821",
+    "5_prime_UTR_variant": "SO:0001623",
+    "synonymous_variant": "SO:0001819",
+    "splice_site_variant": "SO:0001629",
+    "3_prime_UTR_variant": "SO:0001624",
+    "2kb_upstream_variant": "SO:0001636",
+    "2kb_downstream_variant": "SO:0002083",
+    "stop_gained": "SO:0001587",
+    "retained_intron": "SO:0002113",
+    "inframe_deletion": "SO:0001822",
+    "misc_RNA": "SO:0000673",
+    "complex_substitution": "SO:1000005",
+    "processed_transcript": "SO:0001503",
+    "transcribed_unprocessed_pseudogene": "SO:0002107",
+    "unprocessed_pseudogene": "SO:0001760",
+    "miRNA": "SO:0000276",
+    "processed_pseudogene": "SO:0000043",
+    "snRNA": "SO:0000274",
+    "transcribed_processed_pseudogene": "SO:0002109",
+    "NMD_transcript_variant": "SO:0001621",
+    "unconfirmed_transcript": "SO:0002139",
+    "pseudogene": "SO:0000336",
+    "transcribed_unitary_pseudogene": "SO:0002108",
+    "NSD_transcript": "SO:0002130",
+    "snoRNA": "SO:0000275",
+    "scaRNA": "SO:0002095",
+    "unitary_pseudogene": "O:0001759",
+    "polymorphic_pseudogene": "SO:0001841",
+    "rRNA": "SO:0000252",
+    "IG_V_pseudogene": "SO:0002102",
+    "ribozyme": "SO:0000374",
+    "TR_V_gene": "SO:0002137",
+    "TR_V_pseudogene": "SO:0002103",
+    "TR_D_gene": "SO:0002135",
+    "TR_J_gene": "SO:0002136",
+    "TR_C_gene": "SO:0002134",
+    "TR_J_pseudogene": "SO:0002104",
+    "IG_C_gene": "SO:0002123",
+    "IG_C_pseudogene": "SO:0002100",
+    "IG_J_gene": "SO:0002125",
+    "IG_J_pseudogene": "SO:0002101",
+    "IG_D_gene": "SO:0002124",
+    "IG_V_gene": "SO:0002126",
+    "translated_processed_pseudogene": "SO:0002105",
+    "scRNA": "SO:0000013",
+    "vault_RNA": "SO:0000404",
+    "translated_unprocessed_pseudogene": "SO:0002106",
+    "Mt_tRNA": "SO:0002129",
+    "Mt_rRNA": "SO:0002128",
+    "start_retained_variant": "SO:0002019",
+    "stop_retained_variant": "SO:0001567",
+    "exon_loss_variant": "SO:0001572",
+    "transcript_ablation": "SO:0001893",
+    "pseudogene_rRNA": "SO:0002111",
+    "sRNA": "SO:0002352",
+}
+
+chrom_dict = {
+    "chr1": "LA21254-0",
+    "chr2": "LA21255-7",
+    "chr3": "LA21256-5",
+    "chr4": "LA21257-3",
+    "chr5": "LA21258-1",
+    "chr6": "LA21259-9",
+    "chr7": "LA21260-7",
+    "chr8": "LA21261-5",
+    "chr9": "LA21262-3",
+    "chr10": "LA21263-1",
+    "chr11": "LA21264-9",
+    "chr12": "LA21265-6",
+    "chr13": "LA21266-4",
+    "chr14": "LA21267-2",
+    "chr15": "LA21268-0",
+    "chr16": "LA21269-8",
+    "chr17": "LA21270-6",
+    "chr18": "LA21271-4",
+    "chr19": "LA21272-2",
+    "chr20": "LA21273-0",
+    "chr21": "LA21274-8",
+    "chr22": "LA21275-5",
+    "chrX": "LA21276-3",
+    "chrY": "LA21277-1",
+}
+
+
