8ac7bc6b71daaba83407814575833efcfc03f370
Shayne Skrtic <shayneskrtic@shaynes-air.lan>
2024-04-07 11:07:11 -0400

    Updates to the GFF/GVF/GVF3 Convertor

--- converters/gvf-converter/gvf-converter.py
+++ converters/gvf-converter/gvf-converter.py
@@ -3,6 +3,7 @@ from typing import Dict
 from oakvar import BaseConverter
 
 
+
 class Converter(BaseConverter):
 
     def __init__(self, *args, **kwargs):
@@ -23,41 +24,65 @@ class Converter(BaseConverter):
             return False
 
     def convert_line(self, line) -> List[Dict]:
-        ref_base = ''
+        """
+        Converts a line from the file into a structured dictionary.
+        """
+        var_dicts = []
 
-        if line.startswith('#'):
+        if line.startswith('#') or not line.strip():
             return self.IGNORE
 
-        line_values = line.split()
-
-        # chrom is first value
-        chrom_val = line_values[0]
-
-        if 'chr' not in chrom_val:
-            chrom_val = 'chr' + chrom_val
-
-        # pos
+        delimiter = "\t" if self.format_name in ("gff", "gff3") else " "
+        line_values = line.split(delimiter)
+        if len(line_values) < 8:  # Basic validation of line structure
+            return None
+        
+        chrom_val = line_values[0] if 'chr' in line_values[0] else 'chr' + line_values[0]
         pos_val = line_values[3]
+        end_pos = line_values[4]
+        ref_base, alt_base, sample = '', '', 'unknown'  # Initialize with default values
 
-        # ref_base
+
         if self.format_name == 'gvf':
             value_attrs = line_values[-1].split(';')
-
             for val in value_attrs:
                 if 'Reference_seq' in val:
-                    ref_base = val.split('-')[-1]
-                else:
-                    ref_base = '-'
+                    ref_base = val.split('=')[-1]
+                elif 'Variant_seq' in val:
+                    alt_base = val.split('=')[-1]
 
-        var_dicts = []
-        var_dict = {
-            "chrom": chrom_val,
-            "pos": pos_val,
-            "ref_base": ref_base,
-            "alt_base": "T",
-            "sample_id": "sample1",
-            'var_no': self.line_no
-        }
-        var_dicts.append(var_dict)
-        print(var_dict)
-        return var_dicts
+            if "," in alt_base:
+                alt_bases = []
+                alt_bases = alt_base.split(',')
+                for alt in alt_bases:
+                    var_dict = {
+                        "chrom": chrom_val,
+                        "pos": pos_val,
+                        'end_pos': end_pos,
+                        "ref_base": ref_base,
+                        "alt_base": alt,
+                        "sample_id": sample,
+                    }
+                    var_dicts.append(var_dict)
+            else:
+                var_dict = {
+                    "chrom": chrom_val,
+                    "pos": pos_val,
+                    'end_pos': end_pos,
+                    "ref_base": ref_base,
+                    "alt_base": alt_base,
+                    "sample_id": sample,
+                }
+                var_dicts.append(var_dict)
+            return var_dicts
+        elif self.format_name in ("gff", "gff3"):
+            var_dict = {
+                "chrom": chrom_val,
+                "pos": pos_val,
+                'end_pos': end_pos,
+                "ref_base": "!",
+                "alt_base": "!",
+                "sample_id": sample,
+            }
+            var_dicts.append(var_dict)
+            return var_dicts
