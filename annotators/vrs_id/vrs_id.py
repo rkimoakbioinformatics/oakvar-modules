ef0776dc5d16f834bdaa7deda99aff0463ad6991
Nitram36 <100593605+Nitram36@users.noreply.github.com>
2024-04-16 16:14:58 +0100

    calculates vrs alleles for delins
    VRS alleles are successfully calculated for deletion and insertion variants.

--- annotators/vrs_id/vrs_id.py
+++ annotators/vrs_id/vrs_id.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from oakvar import BaseAnnotator
+import oakvar as ov
 
 ## Additional Packages
+import sys
 import warnings
 warnings.simplefilter("ignore") # prevents warning message about JSON schema
 from ga4gh.vrs import models
@@ -9,32 +10,56 @@ from ga4gh.vrs.dataproxy import SeqRepoRESTDataProxy
 from ga4gh.vrs.extras.translator import Translator
 from biocommons.seqrepo import SeqRepo
 
-class Annotator(BaseAnnotator):
+class Annotator(ov.BaseAnnotator):
 
     def annotate(self, input_data: dict, secondary_data: Optional[dict] = None):
-        
+        ## Annotates VCF file.
         assert input_data is not None
         out = {}
         dp = SeqRepoRESTDataProxy(base_url="https://services.genomicmedlab.org/seqrepo")
         ## Translator
         tlr = Translator(data_proxy=dp,
-                         translate_sequence_identifiers=True,  # default
-                         normalize=True,                       # default
-                         identify=True)                        # default
+                         translate_sequence_identifiers=True,  
+                         normalize=True,                       
+                         identify=True)                        
 
-        chrom = input_data['chrom'].strip('chr')
-        pos = input_data['pos'] - 1 # becomes end position in VRS format
+        chrom = input_data['chrom']
+        pos = input_data['pos']
         ref = input_data['ref_base']
         alt = input_data['alt_base']
-        allele = f'{chrom} : {pos} {ref} > {alt}' # get in format of e.g. "19 : 44908822 C > T"
-        vrs_allele = tlr.translate_from(allele, 'beacon') # translate to vrs format
-        computed_id = vrs_allele.as_dict()['_id'] # save allele ID
 
-        out['id'] = computed_id
+        reader = ov.get_wgs_reader("hg38")
+        prev_base = reader.get_bases(chrom, pos - 1).upper() # get previous base for gnomad format
+
+        if ref == '-':
+            ref = prev_base
+            alt = prev_base + alt
+            pos = pos - 1
+
+        if alt == '-':
+            alt = prev_base
+            ref = prev_base + ref
+            pos = pos - 1
+
+        gnomad_expr = f'{chrom}-{pos}-{ref}-{alt}'
+        vrs_allele = tlr.translate_from(gnomad_expr, 'gnomad') # translate gnomad expression to VRS allele
+        vrs_id = vrs_allele.as_dict()['_id']
+
+        out['id'] = vrs_id
         return out
-
 
 if __name__ == '__main__':
     annotator = Annotator(sys.argv)
     annotator.run()
+
+
+
+
+
+
+
+
+
+
+
 
